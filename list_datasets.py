344c0bd5f2341973ab96532446793d25e6b3cb11
silviopiredda <79990768+silviopiredda@users.noreply.github.com>
2022-05-25 17:09:17 +0200

    Add files via upload

---
+++ list_datasets.py
@@ -0,0 +1,4 @@
+datasets = {
+            'blockchain' : './app/PatentAnalysis/Datasets/Blockchain_patent families.csv',
+            'digital twins' : './app/PatentAnalysis/Datasets/Digital Twins.csv'
+           }
